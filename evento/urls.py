c0e72e859df5da7661d9125af27d17d73a404871
simas <simas@localhost.localdomain>
2019-04-22 14:43:35 -0600

    event details added

--- evento/urls.py
+++ evento/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.index, name='evento')
-	
+	path('',views.index, name='evento'),
+	path('detalle/',views.detail, name='detalle'),
 ]
--- evento/views.py
+++ evento/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request,template='Eventos.html'):
 	return render(request, template, locals())
+
+def detail(request,template='eventDetails.html'):
+	return render(request, template,locals())

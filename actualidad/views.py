45a8b981630eac6389e852663efe723283172bcd
simas <simas@localhost.localdomain>
2019-04-23 13:01:08 -0600

    base added to details

--- actualidad/views.py
+++ actualidad/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request,template='actualidad.html'):
 	return render(request, template, locals())
+
+def detail(request, template = 'actualidaDetalle.html'):
+	return render(request, template, locals())
--- foro/views.py
+++ foro/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index (request, template='ForumIndex.html'):
+	return render(request,template,locals())
+
+def detail (request, template = 'ForumDetail.html'):
+	return render(request,template,locals())

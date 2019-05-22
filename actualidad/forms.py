51c897228bef64d309eeeb01bf58a7f817d745a7
Ana Barquero <anabarquero.ab@gmail.com>
2019-05-22 11:38:21 -0600

    admin agregar

--- actualidad/forms.py
+++ actualidad/forms.py
@@ -1,9 +1,23 @@
 from django.db import models
 from .models import *
 from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from embed_video.fields import EmbedVideoField
+from sorl.thumbnail import ImageField
 
-class Actualidad(forms.ModelForm):
+Types_actualidad = (
+	(1,'Informémonos'),
+	(2,'Situación regional de género'),
+	(3,'Reflexiones')
+)
+
+class ActualidadForms(forms.ModelForm):
+	content = forms.CharField(widget=CKEditorUploadingWidget())
+	photo = forms.ImageField(required=True)
+	category = forms.ChoiceField(choices = Types_actualidad)
+	tittle = forms.CharField(widget=forms.TextInput(attrs={'class':'span7','rel':"tooltip", 'title':"Tratar de redactar títulos resumidos"}))
 	class Meta:
-		model: Actualidad
-		
+		model = Actualidad
+		fields = '__all__'
+		exclude = ('slug','created_on','author',)
 			
--- organizaciones/views.py
+++ organizaciones/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import *
 from actualidad.models import *
 from foro.models import *
 from evento.models import *
 from galerias.models import *
+from actualidad.forms import *
 
 # Create your views here.
 
@@ -22,15 +25,55 @@ def actualidad_list(request, template = 'admin/actualidad_index.html'):
 	list_object = Actualidad.objects.filter(author = request.user.id)
 	return render (request, template,locals())
 
+@login_required
+def actualidad_crear(request,template = 'admin/new_actualidad.html'):
+	if request.method == 'POST':
+		form = ActualidadForms(request.POST, request.FILES)
+		if form.is_valid():
+			actualidad = form.save(commit=False)
+			actualidad.author = request.user
+			actualidad.save()
+			form.save_m2m()
+			return HttpResponseRedirect('/contrapartes/actualidad/')
+	else:
+		form = ActualidadForms()
+	return render(request, template, locals())
+
+@login_required
+def actualidad_editar(request, slug, template = 'admin/edit_actualidad.html'):
+	object = get_object_or_404(Actualidad, slug=slug)
+	if request.method == 'POST':
+		form = ActualidadForms(request.POST, request.FILES, instance=object)
+		if form.is_valid():
+			form_uncommited = form.save()
+			form_uncommited.user = request.user
+			form_uncommited.save()
+			return HttpResponseRedirect('/contrapartes/actualidad/')
+	else:
+		form = ActualidadForms(instance=object)
+	return render(request, template, locals())
+
 def foros_list(request, template = 'admin/foros_list.html'):
 	list_object_forum = Foros.objects.filter(usuario = request.user.id)
 	return render(request,template,locals())
+
+def foros_crear(request,template = 'admin/new_foro.html'):
+	return render(request, template, locals())
 
 def events_list(request,template = 'admin/events_list.html'):
 	list_object_events = Evento.objects.filter(author = request.user.id)
 	return render(request,template,locals())
 
-def galeria_imagenes_list(request,template='admin/galeria_list.html'):
+def events_crear(request,template = 'admin/new_event.html'):
+	return render(request, template, locals())
+
+def galeria_list(request,template='admin/galeria_list.html'):
 	list_objects_imagenes = GaleriaImagenes.objects.filter(usuario = request.user.id)
 	list_objects_video = GaleriaVideos.objects.filter(usuario = request.user.id)
 	return render(request,template,locals())
+
+def galeria_img_crear(request,template = 'admin/new_galeria_img.html'):
+	return render(request, template, locals())
+
+def galeria_vid_crear(request,template = 'admin/new_galeria_vid.html'):
+	return render(request, template, locals())

72e89c33f1fb5d0abb20e3ce830a0b9001ef6b17
Erick Murillo <erickmurillo22@gmail.com>
2019-05-31 15:43:43 -0600

    config sitio imgs

--- configuracion/admin.py
+++ configuracion/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from .models import *
+from solo.admin import SingletonModelAdmin
 
 # Register your models here.
 
-class SliderAdmin(admin.ModelAdmin):
+class SliderAdmin(SingletonModelAdmin):
 	fieldsets = [
         ('Slider 1',{'fields':['texto_1','foto_1','credito_1']}),
         ('Slider 2',{'fields':['texto_2','foto_2','credito_2']}),
         ('Slider 3',{'fields':['texto_3','foto_3','credito_3']}),
         ]
 
-admin.site.register(SiteConfiguration)
+admin.site.register(SiteConfiguration,SingletonModelAdmin)
 admin.site.register(Slider,SliderAdmin)

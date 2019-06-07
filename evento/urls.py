37468ff7eb10c2d8d13107fecad76351ded65a2a
Ana Barquero <anabarquero.ab@gmail.com>
2019-06-07 12:49:32 -0600

    templates

--- evento/urls.py
+++ evento/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
 	path('eventos/tag/<slug>',views.filtro_tag_eventos, name ='filtro_tag_eventos'),
 	path('campanias/',views.indexCampanias,name = 'indexCampanias'),
 	path('campanias/<slug>',views.detailCampanias, name = 'detail_campanias'),
+	path('campanias/tag/<slug>/',views.filtro_tag_campanias, name='filtro-tag-camp'),
 	path('concursos/',views.indexConcursos, name = 'indexConcursos'),
 	path('concursos/<slug>', views.detailConcursos, name = 'detailConcursos'),
 	path('concurso/tag/<slug>/',views.filtro_tag_concurso, name='filtro-tag'),
-	path('campanias/tag/<slug>/',views.filtro_tag_campanias, name='filtro-tag-camp'),
 
 ]

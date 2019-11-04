64b06c80c6a6e33f6a313eb5932feb1d2135a3cc
Erick Murillo Molina <erickmurillo22@gmail.com>
2019-11-04 09:02:28 -0600

    cambios login

--- cantera/settings.py
+++ cantera/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.twitter',
+    # 'allauth.socialaccount.providers.twitter',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',

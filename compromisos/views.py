903e5eae350bb270578ecc45bee8956d2a813de8
Erick Murillo <erickmurillo22@gmail.com>
2021-08-31 13:20:27 -0600

    fix compromisos

--- compromisos/views.py
+++ compromisos/views.py
@@ -16,9 +16,12 @@ def get_compromisos(request):
 		dict['hombres'] = comp.conteo_hombres
 		dict['mujeres'] = comp.conteo_mujeres
 		list_fotos = []
-		html = ''
+		fotos = ''
 		for foto in comp.fotoscompromisos_set.all():
-			html += '<div class="vertical-item content-padding big-padding bottom_color_border tooltip_map" style="width: 300px;"><h5 class="">Nicaragua</h5><div class="owl-carousel owl-theme owl-map"><div class="item-media"><a data-fancybox="gallery" href=""><img src="https://picsum.photos/300/224?random=1" alt=""></a></div><div class="item-media"><a data-fancybox="gallery" href=""><img src="https://picsum.photos/300/224?random=2" alt=""></a></div><div class="item-media"><a data-fancybox="gallery" href=""><img src="https://picsum.photos/300/224?random=3" alt=""></a></div><div class="item-media"><a data-fancybox="gallery" href=""><img src="https://picsum.photos/300/224?random=4" alt=""></a></div></div><div class="item-content"><div class="value main_value" akhi="200">0</div><h5 class="">Compromisos</h5><ul class="sex_value"><li>Mujeres: <span class="value" akhi="200">0</span></li><li>Hombres: <span class="value" akhi="200">0</span></li></ul></div></div>'
+			fotos += '<div class="item-media"><a data-fancybox="gallery" href=""><img src='+str(foto.cached_img)+' alt=""></a></div>'
+
+		html = '<div class="vertical-item content-padding big-padding with_border bottom_color_border tooltip_map" style="width: 300px;"><h5 class="">'+comp.get_pais_display()+'</h5><div class="owl-carousel owl-theme owl-map">'+fotos+'</div><div class="item-content"><div class="value main_value" akhi="200">'+str(comp.total)+'</div><h5 class="">Compromisos</h5><ul class="sex_value"><li>Mujeres: <span class="value" akhi="200">'+str(comp.conteo_mujeres)+'</span></li><li>Hombres: <span class="value" akhi="200">'+str(comp.conteo_hombres)+'</span></li></ul></div></div>'
+
 		dict['fotos'] = html
 
 		lista.append(dict)

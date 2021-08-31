4cfcd516700eee9639d276fd3f24235fc6741983
shiminasai <pablo.ismael90@gmail.com>
2021-08-31 11:15:56 -0600

    agragndo diseno a map

--- compromisos/views.py
+++ compromisos/views.py
@@ -18,7 +18,7 @@ def get_compromisos(request):
 		list_fotos = []
 		html = ''
 		for foto in comp.fotoscompromisos_set.all():
-			html += '<img style="width: 100%; height: 100px;" src='+str(foto.cached_img)+' alt=''><br/>'
+			html += '<div class="vertical-item content-padding big-padding bottom_color_border tooltip_map" style="width: 300px;"><h5 class="">Nicaragua</h5><div class="owl-carousel owl-theme owl-map"><div class="item-media"><a data-fancybox="gallery" href=""><img src="https://picsum.photos/300/224?random=1" alt=""></a></div><div class="item-media"><a data-fancybox="gallery" href=""><img src="https://picsum.photos/300/224?random=2" alt=""></a></div><div class="item-media"><a data-fancybox="gallery" href=""><img src="https://picsum.photos/300/224?random=3" alt=""></a></div><div class="item-media"><a data-fancybox="gallery" href=""><img src="https://picsum.photos/300/224?random=4" alt=""></a></div></div><div class="item-content"><div class="value main_value" akhi="200">0</div><h5 class="">Compromisos</h5><ul class="sex_value"><li>Mujeres: <span class="value" akhi="200">0</span></li><li>Hombres: <span class="value" akhi="200">0</span></li></ul></div></div>'
 		dict['fotos'] = html
 
 		lista.append(dict)

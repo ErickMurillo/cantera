9ae3d4dcb0313dc5134326f0384fe9e35ff65a95
Erick Murillo <erickmurillo22@gmail.com>
2026-03-07 15:33:46 -0500

    mapa complete

--- compromisos/urls.py
+++ compromisos/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('get-json/', get_compromisos, name='get-compromisos'),
+    path('get-json/detalle/', get_compromisos_detalle, name='get-compromisos-detalle'),
 ]
--- compromisos/views.py
+++ compromisos/views.py
@@ -4,6 +4,7 @@ import json as simplejson
 from django.core import serializers
 from django.http import HttpResponse
 from django.db.models import Avg, Sum, F, Count, Q
+from . import dataCountries as data
 
 # Create your views here.
 def get_compromisos(request):
@@ -19,18 +20,15 @@ def get_compromisos(request):
 					lista.append(x)
 	except FileNotFoundError:
 		print(f"Error: The file was not found.")
+	return HttpResponse(simplejson.dumps(list(lista)), content_type = 'application/json')
 
-	# for comp in compromisos:
-	# 	dict = {}
-	# 	dict['code3'] = comp.pais
-	# 	dict['value'] = comp.total
-	# 	dict['hombres'] = comp.conteo_hombres
-	# 	dict['mujeres'] = comp.conteo_mujeres
+def get_compromisos_detalle(request):
+	name = request.GET.get('name')
+	lista = []
+	country_display_to_value = dict((v, k) for k, v in data.COUNTRIES_CODE_CHOICES)
+	display_name = name
+	stored_value = country_display_to_value.get(display_name)
 
-	# 	html = '<div class="vertical-item content-padding big-padding with_border bottom_color_border tooltip_map" style="width: 250px;"><h5 class="">'+comp.get_pais_display()+'</h5><div class=""><div class="item-media"><img src='+str(comp.cached_img)+' alt=""></div></div><div class="item-content"><div class="value main_value" akhi="200">'+str(comp.total)+'</div><h5 class="">Compromisos</h5><ul class="sex_value"><li>Mujeres: <span class="value" akhi="200">'+str(comp.conteo_mujeres)+'</span></li><li>Hombres: <span class="value" akhi="200">'+str(comp.conteo_hombres)+'</span></li></ul></div></div>'
-
-	# 	dict['fotos'] = html
-
-	# 	lista.append(dict)
-
+	comp = Compromiso.objects.get(pais = stored_value)
+	lista.append((name,comp.total,comp.conteo_hombres,comp.conteo_mujeres))
 	return HttpResponse(simplejson.dumps(list(lista)), content_type = 'application/json')

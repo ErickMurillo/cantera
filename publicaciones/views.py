62663f23d7d0d374c3a69653c5098f1ee7e060b7
Erick Murillo <erickmurillo22@gmail.com>
2026-03-09 15:19:24 -0400

    fix forms

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -15,9 +15,9 @@ def index_publicaciones(request,template='list_publicacion.html'):
 										Q(resumen__icontains = q) |
 										Q(tematica__nombre__icontains = q) |
 										Q(tags__name__icontains = q),
-										aprobado = True).distinct().order_by('-id')#'id'
+										aprobado = True).distinct('id').order_by('-id')
 	else:
-		list_pub = Publicacion.objects.filter(tipo = 1,aprobado = True).distinct().order_by('-id')#'id'
+		list_pub = Publicacion.objects.filter(tipo = 1,aprobado = True).distinct('id').order_by('-id')
 	hoy = datetime.date.today()
 	prox_eventos = Evento.objects.filter(inicio__gte = hoy,aprobado = True).order_by('-inicio')[:3]
 	ids = list_pub.values_list('id',flat=True)

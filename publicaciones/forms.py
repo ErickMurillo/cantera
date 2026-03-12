8b64649dbb311577636bd042633773af0942c7aa
Erick Murillo <erickmurillo22@gmail.com>
2026-03-12 12:15:16 -0400

    ul remove bullet on detalle publicacion

--- publicaciones/forms.py
+++ publicaciones/forms.py
@@ -29,12 +29,12 @@ class VideosPublicacionForm(forms.ModelForm):
 		fields = '__all__'
 		exclude = ['publicacion',]
 
-PERFIL_CHOICES = (('','---'),(1,'Educador/a o facilitador/a'),(2,'Multiplicador/a comunitario/a'),(3,'Estudiante'),(4,'Integrante de organización social'),
+PERFIL_CHOICES_FORM = (('','---'),(1,'Educador/a o facilitador/a'),(2,'Multiplicador/a comunitario/a'),(3,'Estudiante'),(4,'Integrante de organización social'),
 				  (5,'Investigador/a o académico/a'),(6,'Funcionaria/o público'),(7,'Integrante de ONG o cooperación internacional'),
-				  (8,'Consultor/a o profesional independiente'),(9,'Persona interesada en temas de género'),(10,'Otro'))
+				  (8,'Consultor/a o profesional independiente'),(9,'Persona interesada en temas de género'))
 
 class PreguntasForm(forms.Form):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.fields['utilizara_material'] = forms.ModelMultipleChoiceField(queryset=RespMaterial.objects.all(),widget=forms.CheckboxSelectMultiple)
-		self.fields['perfil'] = forms.ChoiceField(choices=PERFIL_CHOICES)
+		self.fields['utilizara_material'] = forms.ModelMultipleChoiceField(queryset=RespMaterial.objects.all(),widget=forms.CheckboxSelectMultiple,label="¿Para qué utilizará este material? (Puede seleccionar una o más opciones)'")
+		self.fields['perfil'] = forms.ChoiceField(choices=PERFIL_CHOICES_FORM,label='¿Cuál es su perfil?')
